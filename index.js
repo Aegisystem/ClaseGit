f4d3a59687464bf86a4f0858895ad1ed3707ddd3
Steven Correa <steven14correa@gmail.com>
2025-10-27 12:42:44 -0500

    Inicializacion del proyecto

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const express = require('express');
+
+const app = express();
+const port = 3000 || process.env.PORT;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hola Mundo');
+});
+
+app.listen(port, () => {
+  console.log(`Corriendo`);
+})
