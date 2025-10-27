79b531b1b48a45f4a9ac8cd76ca753c59f0308ca
Steven Correa <steven14correa@gmail.com>
2025-10-27 13:31:01 -0500

    Consumiendo api de comics

--- controllers/comics.js
+++ controllers/comics.js
@@ -1,7 +1,21 @@
 const comicsController = {}
+const axios = require('axios');
 
-comicsController.getComics = (req, res) => {
-    res.json({ message: 'Lista de comics' });
+comicsController.getComics = async (req, res) => {
+    
+    const APIKEY = process.env.APIKEY;
+    const TS = process.env.TS;
+    const HASH = process.env.HASH;
+    const MARVEL_API = process.env.MARVEL_API;
+
+    await axios.get(`${MARVEL_API}?ts=${TS}&apikey=${APIKEY}&hash=${HASH}`)
+        .then(response => {
+            res.json(response.data);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    console.log('Obteniendo comics...');
 }
 
 module.exports = comicsController;
