fff579d1eec2b6c6ce4af50c30fb885525e45067
Steven Correa <steven14correa@gmail.com>
2025-10-27 13:08:14 -0500

    Implementacion comics

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const port = 3000 || process.env.PORT;
 
 app.use(express.json());
 
+app.use('/comics', require('./routes/comics'));
+
 app.listen(port, () => {
-  console.log(`Corriendo`);
+  console.log(`Corriendo en el puerto ${port}`);
 })
